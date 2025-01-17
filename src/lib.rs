ea152054f37f3494338a5e51cecda2d6b82b7901
bbking <b@bbki.ng>
2025-01-17 16:32:01 +0800

    test ui

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, ToBytes, FromBytes)]
+#[encoding(Json)]
+pub struct Stats {
+    pub tag: String,
+    pub name: String,
+    pub leagueIcon: String,
+}
+
+impl Stats {
+    fn to_html(&self) -> String {
+        format!("
+            <li>{}</li>
+            <li>{}</li>
+        ", self.name, self.tag)
+    }
+}
+
+fn fetch_stats(api: &String) -> FnResult<Stats> {
+    let req = HttpRequest{
+        url: (&api).to_string(),
+        method: Some("GET".to_string()),
+        headers: BTreeMap::new(),
+    };
+
+    let res = http::request::<()>(&req, None)?;
+    Ok(res.json::<Stats>()?)
+}
+
+#[plugin_fn]
+pub fn ui() -> FnResult<String> {
+    let api = String::from("https://api.bbki.ng/coc");
+    let res = fetch_stats(&api);
+    
+    Ok(res.to_html)
+}
+
+
 // start with something simple
 #[plugin_fn]
 pub fn coc() -> FnResult<String> {
