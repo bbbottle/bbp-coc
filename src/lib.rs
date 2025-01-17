5b3694382648be76eac2118e1389fdf26154b499
bbking <b@bbki.ng>
2025-01-17 07:53:59 +0000

    chore: test version

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ use serde::{Deserialize, Serialize};
 // start with something simple
 #[plugin_fn]
 pub fn coc() -> FnResult<String> {
-    Ok(format!("Work In Progress v0.0.2"))
+    Ok(format!("Work In Progress v0.0.3"))
 }
