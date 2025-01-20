6c317b25c762d30e040a241baab5c79782dd0349
bbking <b@bbki.ng>
2025-01-20 06:24:22 +0000

    fix: remove ui code

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,6 @@ use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
 
-mod ui;
-
-#[plugin_fn]
-pub fn init() -> FnResult<String> {
-    // Generate the HTML and JavaScript content
-    let content = ui::generate_ui();
-    Ok(content)
-}
-
-
 // ------------------old code------------------
 
 #[derive(Serialize, Deserialize, ToBytes, FromBytes)]
