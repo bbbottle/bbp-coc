e3cd36227a4d187ce92f31c621d2c2543aa8f5f9
bbking <b@bbki.ng>
2025-01-20 09:21:29 +0000

    fix: btn render

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,12 @@ use std::collections::BTreeMap;
 use leptos::*;
 #[component]
 pub fn HelloButton() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| {
-        set_count.update(|n| *n + 1);
-    };
-
     view! {
         <div class="hello-container">
             <button 
                 class="hello-btn"
-                on:click=on_click
             >
-                "Hello World! (clicked " {count} " times)"
+                "Hello World!"
             </button>
         </div>
     }
